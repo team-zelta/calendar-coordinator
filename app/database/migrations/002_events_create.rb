77d4c30212c48c530b8d6f80c5de320f9015662f
tienyulin <t.tienyulin@gmail.com>
2021-04-25 21:55:17 +0800

    feat(database): modify key setting for event

--- app/database/migrations/002_events_create.rb
+++ app/database/migrations/002_events_create.rb
@@ -5,7 +5,8 @@ require 'sequel'
 Sequel.migration do
   change do
     create_table(:events) do
-      primary_key :id
+      String :id, primary_key: true
+      foreign_key :calendar_id, table: :calendars
 
       String :status
       String :summary, null: false
