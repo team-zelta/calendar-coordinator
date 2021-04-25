57a2bab97365e1a4fc5b26fcd11e57c2e144ed13
tienyulin <t.tienyulin@gmail.com>
2021-04-25 21:55:58 +0800

    feat(calendar): add database setting

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,25 +1,21 @@
 # frozen_string_literal: true
 
 require 'json'
-require 'base64'
-require 'rbnacl'
+require 'sequel'
 
 # CalendarCoordinator
 module CalendarCoordinator
   # Calendar Class
-  class Calendar
-    STORE_DIR = 'app/database/store/calendars'
+  class Calendar < Sequel::Model
+    # Enable primary key setter
+    unrestrict_primary_key
 
-    def initialize(calendar)
-      @id = calendar['id'] || new_id
-      @summary = calendar['summary']
-      @description = calendar['description']
-      @location = calendar['location']
-      @time_zone = calendar['timeZone']
-      @access_role = calendar['accessRole']
-    end
+    # Defind relationships between models
+    one_to_many :event
+    plugin :association_dependencies, event: :destroy
 
-    attr_reader :id, :summary, :description, :location, :time_zone, :access_role
+    # Auto set created_at & updated_at
+    plugin :timestamps
 
     def to_json(options = {}) # rubocop:disable Metrics/MethodLength
       JSON(
@@ -33,37 +29,6 @@ module CalendarCoordinator
         },
         options
       )
-    end
-
-    # Setup store directory
-    def self.setup
-      Dir.mkdir(STORE_DIR) unless Dir.exist? STORE_DIR
-    end
-
-    # Save Calendar
-    def save
-      File.write("#{STORE_DIR}/#{id}.txt", to_json)
-    end
-
-    # Find Calendar by id
-    def self.find(id)
-      calendar = File.read("#{STORE_DIR}/#{id}.txt")
-      MyCalendar.new(JSON.parse(calendar))
-    end
-
-    # Find all Calendars
-    def self.all
-      Dir.glob("#{STORE_DIR}/*.txt").map do |file|
-        file.match(%r{#{Regexp.quote(STORE_DIR)}/(.*)\.txt})[1]
-      end
-    end
-
-    private
-
-    # Create new id
-    def new_id
-      timestamp = Time.now.to_f.to_s
-      Base64.urlsafe_encode64(RbNaCl::Hash.sha256(timestamp))[0..9]
     end
   end
 end
