8c127cf0a14310d0de9606252487ef2309bfd8a4
tienyulin <t.tienyulin@gmail.com>
2021-04-25 21:56:52 +0800

    feat(api): modify calendar and event route & set

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -29,7 +29,7 @@ module CalendarCoordinator
               # GET /api/v1/calendars/{calendar_id}/events/{event_id}
               routing.get String do |event_id|
                 response.status = 200
-                event = Event.where(calendar_id: calendar_id, event_id: event_id).first
+                event = Event.where(calendar_id: calendar_id, id: event_id).first
                 event ? event.to_json : raise('Event not found')
               rescue StandardError => e
                 routing.halt 404, { message: e.message }.to_json
@@ -46,7 +46,8 @@ module CalendarCoordinator
               # POST /api/v1/calendars/{calendar_id}/events
               routing.post do
                 data = JSON.parse(routing.body.read)
-                event = Event.create(data)
+                calendar = Calendar.first(id: calendar_id)
+                event = calendar.add_event(data)
 
                 if event
                   response.status = 201
@@ -63,31 +64,34 @@ module CalendarCoordinator
           # GET /api/v1/calendars/{id}
           routing.get String do |calendar_id|
             response.status = 200
-            Calendar.find(calendar_id).to_json
-          rescue StandardError
-            routing.halt 404, { message: 'Calendar not found' }.to_json
+            calendar = Calendar.first(id: calendar_id)
+            calendar ? calendar.to_json : raise('Calendar not found')
+          rescue StandardError => e
+            routing.halt 404, { message: e.message }.to_json
           end
 
           # GET /api/v1/calendars
           routing.get do
             response.status = 200
             JSON.pretty_generate(Calendar.all)
-          rescue StandardError
-            routing.halt 500, { message: 'Server error' }.to_json
+          rescue StandardError => e
+            routing.halt 500, { message: e.message }.to_json
           end
 
           # POST /api/v1/calendars
           routing.post do
             # puts "body: #{JSON.parse(routing.body.read)}"
             data = JSON.parse(routing.body.read)
-            calendar = Calendar.new(data)
+            calendar = Calendar.create(data)
 
-            if calendar.save
+            if calendar
               response.status = 201
               { message: 'Calendar saved', calendar_id: calendar.id }.to_json
             else
               routing.halt 400, { message: 'Save Calendar failed' }.to_json
             end
+          rescue StandardError => e
+            routing.halt 500, { message: e.message }.to_json
           end
         end
       end
